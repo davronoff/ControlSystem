61721ef21b004c4721dd4db18dcf2e5e7fcf28a4
davronoff <ilyosbekdavronov009@gmail.com>
2022-02-11 16:20:06 +0300

    db context

--- ProjectDavomat.Data/DataLayer/DavomatDbContext.cs
+++ ProjectDavomat.Data/DataLayer/DavomatDbContext.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using ProjectDavomat.Domain;
+
 namespace ProjectDavomat.Data
 {
-    public class DavomatDbContext
+    public class DavomatDbContext: DbContext
     {
-        
+        public DavomatDbContext(DbContextOptions<DavomatDbContext>options)
+            :base(options)
+        {
+
+        }
+        public DbSet<Oqituvchi> teachers {get; set;}
+        public DbSet<Oquvchi> puples { get; set; }
     }
 }
