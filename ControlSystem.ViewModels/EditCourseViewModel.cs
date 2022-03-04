5a71b89bf3ba31af510b9949ba67630ec29267eb
davronoff <ilyosbekdavronv009@gmail.com>
2022-03-04 19:10:32 +0500

    editvm

---
+++ ControlSystem.ViewModels/EditCourseViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectDavomat.ViewModels
+{
+    public class EditCourseViewModel
+    {
+        [Required, Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Price { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Duration { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Image { get; set; }
+        public IFormFile NewImage { get; set; }
+        public Guid CourseCategoryId { get; set; }
+    }
+}
