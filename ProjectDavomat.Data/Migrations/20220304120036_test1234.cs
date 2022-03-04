1e6ae66e43b22f01a3080c583cbc626ac8225852
Nodirbek-Abdulaxadov <nbkabdulaxadov@gmail.com>
2022-03-04 17:04:10 +0500

    sffsd

---
+++ ProjectDavomat.Data/Migrations/20220304120036_test1234.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectDavomat.Data.Migrations
+{
+    public partial class test1234 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Price",
+                table: "courses",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "courses",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "courses");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Price",
+                table: "courses",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
