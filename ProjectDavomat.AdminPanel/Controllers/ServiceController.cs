f5e6437703e9942005bd8d6d1731a5ad31425189
davronoff <ilyosbekdavronov009@gmail.com>
2022-03-04 19:59:47 +0300

    some

--- ProjectDavomat.AdminPanel/Controllers/ServiceController.cs
+++ ProjectDavomat.AdminPanel/Controllers/ServiceController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProjectDavomat.BL.Interface;
+using ProjectDavomat.Domain;
+using ProjectDavomat.ViewModels;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ProjectDavomat.AdminPanel.Controllers
@@ -14,14 +18,42 @@ namespace ProjectDavomat.AdminPanel.Controllers
         }
         public  async Task<IActionResult> Services()
         {
-            var item = await _serviceInterface.GetAllService();
-            return View(item);
+            var list = await _serviceInterface.GetAllService();
+            List<AddServiceViewModel> viewModels = new List<AddServiceViewModel>();
+            foreach (var item in list)
+            {
+                viewModels.Add((AddServiceViewModel)item);
+            }
+
+            return View(viewModels);
         }
         [HttpGet]
-        public async Task<IActionResult> AddServices()
+        public  IActionResult AddServices()
         {
             return View();
         }
-        
+        [HttpPost]
+        public async Task<IActionResult> AddService(AddServiceViewModel newViewModel)
+        {
+            Service newSerice = await _serviceInterface.AddService((Service)newViewModel);
+            return RedirectToAction("Service");
+        }
+        public IActionResult Delete(Guid id)
+        {
+            _serviceInterface.DeleteService(id);
+            return RedirectToAction("Service");
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            AddServiceViewModel viewModel = (AddServiceViewModel)await _serviceInterface.GetService(id);
+            return View(viewModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(AddServiceViewModel _editViewModel)
+        {
+            await _serviceInterface.UpdateService((Service)_editViewModel);
+            return RedirectToAction("Service");
+        }
     }
 }
