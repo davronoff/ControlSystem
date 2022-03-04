e9dc77479e6dc8dbe5eba9925e48f8ed70c9aed4
davronoff <ilyosbekdavronv009@gmail.com>
2022-03-04 21:39:17 +0500

    pushshsh

--- ProjectDavomat.Data/Migrations/20220304163257_test1234576.Designer.cs
+++ ProjectDavomat.Data/Migrations/20220304163257_test1234576.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ProjectDavomat.Data.DataLayer;
@@ -9,9 +10,10 @@ using ProjectDavomat.Data.DataLayer;
 namespace ProjectDavomat.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220304163257_test1234576")]
+    partial class test1234576
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
