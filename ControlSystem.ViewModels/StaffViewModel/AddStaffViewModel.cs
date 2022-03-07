07907e658b4564a9c241c47c04df46fc3cf4b113
davronoff <ilyosbekdavronov009@gmail.com>
2022-03-07 13:27:18 +0300

    some chang

--- ControlSystem.ViewModels/StaffViewModel/AddStaffViewModel.cs
+++ ControlSystem.ViewModels/StaffViewModel/AddStaffViewModel.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjectDavomat.ViewModels.Staff
+namespace ProjectDavomat.ViewModels
 {
     public class AddStaffViewModel
     {
--- ProjectDavomat.AdminPanel/Controllers/StaffController.cs
+++ ProjectDavomat.AdminPanel/Controllers/StaffController.cs
@@ -1,16 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjectDavomat.AdminPanel.Services;
 using ProjectDavomat.BL.Interface;
-using ProjectDavomat.ViewModels.Staff;
+using ProjectDavomat.Domain;
+using ProjectDavomat.ViewModels;
+using System;
 using System.Threading.Tasks;
 
 namespace ProjectDavomat.AdminPanel.Controllers
 {
     public class StaffController : Controller
     {
+        private readonly IDeleteSaveimageInterface _deleteSaveimage;
         private readonly IStaffInterface _staffInterface;
 
-        public StaffController(IStaffInterface staffInterface)
+        public StaffController(IStaffInterface staffInterface,
+            IDeleteSaveimageInterface deleteSaveimage)
         {
+            _deleteSaveimage = deleteSaveimage;
             _staffInterface = staffInterface;
         }
         public async Task<IActionResult> Staffs()
@@ -19,11 +25,49 @@ namespace ProjectDavomat.AdminPanel.Controllers
             return View(itam);
         }
         [HttpGet]
-        public  IActionResult AddStaff()
+        public IActionResult AddStaff()
         {
             return View();
         }
-        //[HttpPost]
-        //public async Task<IActionResult> AddStaff(AddStaffViewModel)
+        [HttpPost]
+        public async Task<IActionResult> AddStaff(AddStaffViewModel viewModel)
+        {
+            Staff staff = new Staff()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = viewModel.FirstName,
+                LastName = viewModel.LastName,
+                Image = viewModel.Image,
+                About = viewModel.About,
+                Position = viewModel.Position
+            };
+            await _staffInterface.AddStaff(staff);
+            return RedirectToAction("Staffs");
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var item = await _staffInterface.GetStaff(id);
+            return View((EditStaffViewModel)item);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditStaffViewModel viewmodel)
+        {
+            if(viewmodel.NewImage is not null)
+            {
+                _deleteSaveimage.DeleteImage(viewmodel.Image);
+                viewmodel.Image = _deleteSaveimage.SaveImage(viewmodel.NewImage);
+            }
+            var item = await _staffInterface.UpdateStaff((Staff)viewmodel);
+            return RedirectToAction("Staffs");
+        }
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var item = await _staffInterface.GetStaff(id);
+            _deleteSaveimage.DeleteImage(item.Image);
+            await _staffInterface.DeleteStaff(id);
+            return RedirectToAction("Staffs");
+        }
     }
 }
