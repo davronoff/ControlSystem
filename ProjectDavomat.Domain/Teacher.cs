2975db06e29e0b0e7534536c668f2d336a1f6d3e
davronoff <ilyosbekdavronov009@gmail.com>
2022-03-09 15:59:15 +0300

    add social ot teacher

--- ProjectDavomat.Domain/Teacher.cs
+++ ProjectDavomat.Domain/Teacher.cs
@@ -19,6 +19,7 @@ namespace ProjectDavomat.Domain
          public string Image { get; set; }
          [Required]
          public string Experince { get; set; }
+        public string Social { get; set; }
 
     }
  }
